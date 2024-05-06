82e6af5404022937f86b49d0a48e53f6f64de605
Thiago Matos <thiago@lepidus.com.br>
2024-05-06 19:42:58 -0400

    Dynamically change altmetric badge style
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#820
    
    Signed-off-by: Thiago Matos <thiago@lepidus.com.br>

--- BadgesPlugin.php
+++ BadgesPlugin.php
@@ -2,15 +2,16 @@
 
 /**
  * @file plugins/generic/badges/BadgesPlugin.inc.php
- * 
- * Copyright 2019 
+ *
+ * Copyright 2019
  * Portal de Revistas de la Universidad Nacional de La Plata
- *  https://revistas.unlp.edu.ar 
+ *  https://revistas.unlp.edu.ar
  *  https://sedici.unlp.edu.ar
  *
  * @author gonetil
  *
  * @class badges
+ *
  * @ingroup plugins_generic_dates
  *
  * @brief badges plugin class
@@ -18,148 +19,161 @@
 
 namespace APP\plugins\generic\badges;
 
-use PKP\plugins\GenericPlugin;
-use PKP\plugins\Hook;
 use APP\core\Application;
 use APP\template\TemplateManager;
+use PKP\core\JSONMessage;
 use PKP\linkAction\LinkAction;
 use PKP\linkAction\request\AjaxModal;
-use PKP\core\JSONMessage;
-use APP\plugins\generic\badges\BadgesSettingsForm;
+use PKP\plugins\GenericPlugin;
+use PKP\plugins\Hook;
 
-class BadgesPlugin extends GenericPlugin {
-	/**
-	 * Called as a plugin is registered to the registry
-	 * @param $category String Name of category plugin was registered to
-	 * @return boolean True iff plugin initialized successfully; if false,
-	 * 	the plugin will not be registered.
-	 */
-	function register($category, $path, $mainContextId = NULL) {
-		$success = parent::register($category, $path, $mainContextId);
-		if (Application::isUnderMaintenance()) {
+class BadgesPlugin extends GenericPlugin
+{
+    /**
+     * Called as a plugin is registered to the registry
+     *
+     * @param $category String Name of category plugin was registered to
+     * @param null|mixed $mainContextId
+     *
+     * @return boolean True iff plugin initialized successfully; if false,
+     * 	the plugin will not be registered.
+     */
+    public function register($category, $path, $mainContextId = null)
+    {
+        $success = parent::register($category, $path, $mainContextId);
+        if (Application::isUnderMaintenance()) {
             return $success;
         }
-		if ($success && $this->getEnabled($mainContextId)) {
-			// Insert Badges div
-			Hook::add('Templates::Article::Details', [$this, 'addBadges']);
-			Hook::add('Templates::Preprint::Details', [$this, 'addBadges']);
-		}
-		return $success;
-	}
-
-	/**
-	 * Get the plugin display name.
-	 * @return string
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.badges.displayName');
-	}
-
-	/**
-	 * Get the plugin description.
-	 * @return string
-	 */
-	function getDescription() {
-		return __('plugins.generic.badges.description');
-	}
-
-	private function getDoi($smarty) {
-		$application = Application::getName();
-		$mapAppToVarName = ['ojs2' => 'article', 'ops' => 'preprint'];
-		
-		$submission = $smarty->getTemplateVars($mapAppToVarName[$application]);
-		$publication = $submission->getCurrentPublication();
-
-		return $publication->getDoi();
-	}
-
-	/**
-	 * Add badges to article/preprint landing page
-	 * @param $hookName string
-	 * @param $params array
-	 */
-	function addBadges($hookName, $params) {
-		$request = $this->getRequest();
-		$context = $request->getContext();
-
-		$smarty =& $params[1];
-		$output =& $params[2];
-
-		$doi = $this->getDoi($smarty);
-		$smarty->assign('doi', $doi);
-
-		$badgesShowDimensions = $this->getSetting($context->getId(), 'badgesShowDimensions');
-		$badgesDimensionsHideWhenEmpty = $this->getSetting($context->getId(), 'badgesDimensionsHideWhenEmpty');
-		$badgesShowAltmetric = $this->getSetting($context->getId(), 'badgesShowAltmetric');
-		$badgesAltmetricHideWhenEmpty = $this->getSetting($context->getId(), 'badgesAltmetricHideWhenEmpty');
-		$badgesShowPlumx = $this->getSetting($context->getId(), 'badgesShowPlumx');
-		$badgesPlumxHideWhenEmpty = $this->getSetting($context->getId(), 'badgesPlumxHideWhenEmpty');
-
-		if ($badgesShowDimensions == "on") {
-			$smarty->assign("showDimensions", "true");
-			$smarty->assign("badgesDimensionsHideWhenEmpty",  ( ($badgesDimensionsHideWhenEmpty == "on")? "true" : "false") );
-		}
-		if ($badgesShowAltmetric == "on") {
-			$smarty->assign("showAltmetric","true");
-			$smarty->assign("badgesAltmetricHideWhenEmpty",  ( ($badgesAltmetricHideWhenEmpty == "on")? "true" : "false") );
-		}
-		if ($badgesShowPlumx == "on") {
-			$smarty->assign("showPlumx", "true");
-			$smarty->assign("badgesPlumxHideWhenEmpty",  ( ($badgesPlumxHideWhenEmpty == "on")? "true" : "false") );
-		}
-		$output .= $smarty->fetch($this->getTemplateResource('badges.tpl'));
-		return false;		
-
-	}
-
-	/**
-	 * @copydoc Plugin::getActions()
-	 */
-	function getActions($request, $verb) {
-		$router = $request->getRouter();
-		return array_merge(
-			$this->getEnabled()?array(
-				new LinkAction(
-					'settings',
-					new AjaxModal(
-						$router->url($request, null, null, 'manage', null, array('verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'generic')),
-						$this->getDisplayName()
-					),
-					__('manager.plugins.settings'),
-					null
-				),
-			):array(),
-			parent::getActions($request, $verb)
-		);
+        if ($success && $this->getEnabled($mainContextId)) {
+            // Insert Badges div
+            Hook::add('Templates::Article::Details', [$this, 'addBadges']);
+            Hook::add('Templates::Preprint::Details', [$this, 'addBadges']);
+        }
+        return $success;
     }
-    
 
     /**
-	 * @copydoc Plugin::manage()
-	 */
-	function manage($args, $request) {
-		switch ($request->getUserVar('verb')) {
-			case 'settings':
-				$context = $request->getContext();
+     * Get the plugin display name.
+     *
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.generic.badges.displayName');
+    }
 
-				$templateMgr = TemplateManager::getManager($request);
-				$templateMgr->registerPlugin('function', 'plugin_url', array($this, 'smartyPluginUrl'));
+    /**
+     * Get the plugin description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return __('plugins.generic.badges.description');
+    }
 
-				$form = new BadgesSettingsForm($this, $context->getId());
+    private function getDoi($smarty)
+    {
+        $application = Application::getName();
+        $mapAppToVarName = ['ojs2' => 'article', 'ops' => 'preprint'];
 
-				if ($request->getUserVar('save')) {
-					$form->readInputData();
-					if ($form->validate()) {
-						$form->execute();
-						return new JSONMessage(true);
-					}
-				} else {
-					$form->initData();
-				}
-				return new JSONMessage(true, $form->fetch($request));
-		}
-		return parent::manage($args, $request);
-	}
+        $submission = $smarty->getTemplateVars($mapAppToVarName[$application]);
+        $publication = $submission->getCurrentPublication();
+
+        return $publication->getDoi();
+    }
+
+    /**
+     * Add badges to article/preprint landing page
+     *
+     * @param $hookName string
+     * @param $params array
+     */
+    public function addBadges($hookName, $params)
+    {
+        $request = $this->getRequest();
+        $context = $request->getContext();
+
+        $smarty = & $params[1];
+        $output = & $params[2];
+
+        $doi = $this->getDoi($smarty);
+        $smarty->assign('doi', $doi);
+
+        $badgesShowDimensions = $this->getSetting($context->getId(), 'badgesShowDimensions');
+        $badgesDimensionsHideWhenEmpty = $this->getSetting($context->getId(), 'badgesDimensionsHideWhenEmpty');
+        $badgesShowAltmetric = $this->getSetting($context->getId(), 'badgesShowAltmetric');
+        $badgesAltmetricStyle = $this->getSetting($context->getId(), 'badgesAltmetricStyle');
+        $badgesAltmetricHideWhenEmpty = $this->getSetting($context->getId(), 'badgesAltmetricHideWhenEmpty');
+        $badgesShowPlumx = $this->getSetting($context->getId(), 'badgesShowPlumx');
+        $badgesPlumxHideWhenEmpty = $this->getSetting($context->getId(), 'badgesPlumxHideWhenEmpty');
+
+        if ($badgesShowDimensions == 'on') {
+            $smarty->assign('showDimensions', 'true');
+            $smarty->assign('badgesDimensionsHideWhenEmpty', (($badgesDimensionsHideWhenEmpty == 'on') ? 'true' : 'false'));
+        }
+        if ($badgesShowAltmetric == 'on') {
+            $smarty->assign('showAltmetric', 'true');
+            $smarty->assign('badgesAltmetricStyle', $badgesAltmetricStyle ?? 'donut');
+            $smarty->assign('badgesAltmetricHideWhenEmpty', (($badgesAltmetricHideWhenEmpty == 'on') ? 'true' : 'false'));
+        }
+        if ($badgesShowPlumx == 'on') {
+            $smarty->assign('showPlumx', 'true');
+            $smarty->assign('badgesPlumxHideWhenEmpty', (($badgesPlumxHideWhenEmpty == 'on') ? 'true' : 'false'));
+        }
+        $output .= $smarty->fetch($this->getTemplateResource('badges.tpl'));
+        return false;
+
+    }
+
+    /**
+     * @copydoc Plugin::getActions()
+     */
+    public function getActions($request, $verb)
+    {
+        $router = $request->getRouter();
+        return array_merge(
+            $this->getEnabled() ? [
+                new LinkAction(
+                    'settings',
+                    new AjaxModal(
+                        $router->url($request, null, null, 'manage', null, ['verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'generic']),
+                        $this->getDisplayName()
+                    ),
+                    __('manager.plugins.settings'),
+                    null
+                ),
+            ] : [],
+            parent::getActions($request, $verb)
+        );
+    }
+
+
+    /**
+     * @copydoc Plugin::manage()
+     */
+    public function manage($args, $request)
+    {
+        switch ($request->getUserVar('verb')) {
+            case 'settings':
+                $context = $request->getContext();
+
+                $templateMgr = TemplateManager::getManager($request);
+                $templateMgr->registerPlugin('function', 'plugin_url', [$this, 'smartyPluginUrl']);
+
+                $form = new BadgesSettingsForm($this, $context->getId());
+
+                if ($request->getUserVar('save')) {
+                    $form->readInputData();
+                    if ($form->validate()) {
+                        $form->execute();
+                        return new JSONMessage(true);
+                    }
+                } else {
+                    $form->initData();
+                }
+                return new JSONMessage(true, $form->fetch($request));
+        }
+        return parent::manage($args, $request);
+    }
 }
-
-?>
--- templates/badges.tpl
+++ templates/badges.tpl
@@ -1,9 +1,9 @@
 {**
  * plugins/generic/badges/templates/articleFooter.tpl
  *
- * Copyright 2019 
+ * Copyright 2019
  * Portal de Revistas de la Universidad Nacional de La Plata
- *  https://revistas.unlp.edu.ar 
+ *  https://revistas.unlp.edu.ar
  *  https://sedici.unlp.edu.ar
  *
  * @author gonetil
@@ -14,7 +14,7 @@
     <div class="item badges" style="display: none">
         <h2 class="label">{translate key="plugins.generic.badges.manager.settings.showBlockTitle"}</h2>
 
-        {if $showDimensions}        
+        {if $showDimensions}
         <div class="sub_item">
             <span class="__dimensions_badge_embed__" data-doi="{$doi|escape}" data-hide-zero-citations="{$badgesDimensionsHideWhenEmpty|escape}" data-style="small_circle"></span><script async src="https://badge.dimensions.ai/badge.js" charset="utf-8"></script>
         </div>
@@ -23,7 +23,7 @@
         {if $showAltmetric}
         <div class="sub_item">
             <script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>
-            <div data-badge-popover="right" data-badge-type="donut" data-doi="{$doi|escape}" data-hide-no-mentions="{$badgesAltmetricHideWhenEmpty|escape}" class="altmetric-embed"></div>
+            <div data-badge-popover="right" data-badge-type="{$badgesAltmetricStyle|escape}" data-doi="{$doi|escape}" data-hide-no-mentions="{$badgesAltmetricHideWhenEmpty|escape}" class="altmetric-embed"></div>
         </div>
         {/if}
 
@@ -31,7 +31,7 @@
         <div class="sub_item">
             <script type="text/javascript" src="//cdn.plu.mx/widget-popup.js"></script>
             <a href="https://plu.mx/plum/a/?doi={$doi|escape}" class="plumx-plum-print-popup" data-hide-when-empty="{$badgesPlumxHideWhenEmpty|escape}"></a>
-        </div>   
+        </div>
         {/if}
     </div>
 
